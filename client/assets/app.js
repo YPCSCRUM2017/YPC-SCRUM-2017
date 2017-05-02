aad617c9859ce453c1f6d70c8b13085dd99d23f0
Benjamin Huang <benjaminthuang@gmail.com>
2017-05-02 15:53:03 -0700

    links loading

--- client/assets/app.js
+++ client/assets/app.js
@@ -2,15 +2,40 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider){
 $routeProvider
-  .when('/', {
-      templateUrl: '/partials/home.html',
-      controller: 'main_controller'
-  })
-  .when('/blog',{
-      templateUrl: '../partials/blog.html',
-      controller:'main_controller'
-  })
-  .otherwise({
-      redirectTo : '/'
-  });
+        .when('/', {
+            templateUrl: '/partials/home.html',
+            // controller: 'main_controller'
+        })
+        .when('/blog',{
+
+                templateUrl: '../partials/blog.html',
+                // controller:'blog_controller'
+
+              })
+        .when('/events',{
+              templateUrl: '../partials/events.html',
+              // controller:'blog_controller'
+
+            })
+        .when('/execs',{
+
+                templateUrl: '../partials/executives.html',
+                // controller:'blog_controller'
+
+              })
+        .when('/donate',{
+
+                templateUrl: '../partials/donate.html',
+                // controller:'blog_controller'
+
+              })
+        .when('/contact',{
+
+                templateUrl: '../partials/contact.html',
+                // controller:'blog_controller'
+
+              })
+        .otherwise({
+            redirectTo : '/'
+        });
 });
