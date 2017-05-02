6f593755d4fa6ff531f193abebe4a2961b9c6920
Kamran Mohammed Abid <kamranabid@Kamrans-MacBook-Pro.local>
2017-05-02 14:13:04 -0700

    working on ejs to angular

--- client/assets/app.js
+++ client/assets/app.js
@@ -2,10 +2,16 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider){
 $routeProvider
+  .when('/', {
+      templateUrl: '/partials/home.html',
+      controller: 'main_controller'
+  })
   .when('/blog',{
-          console.log("HIIIIIIIIIIIIII");
-          templateUrl: '../partials/blog.html',
-          controller:'blog_controller'
-
-        })
+      console.log();("HIIIIIIIIIIIIII"),
+      templateUrl: '../partials/blog.html',
+      controller:'main_controller'
+  })
+  .otherwise({
+      redirectTo : '/'
+  });
 });
---
+++ client/assets/controllers/main_controller.js
@@ -0,0 +1,4 @@
+app.controller('main_controller', function($scope, $route){
+  console.log('main controller loaded');
+  }
+})
